dfd811e0d5aa36f021dd4f6c37e4eefa0ac252d9
sapphire2424 <deinelle0611@gmail.com>
2025-05-12 20:48:45 +0900

    エラーハンドリング機能を追加

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import requests
 url = "https://google.com/"
 
 #web情報を取得
-response = requests.get(url) #urlを適宜変更
-soup = BeautifulSoup(response.content, 'html.parser')
+try:
+    response = requests.get(url) #urlを適宜変更
+    soup = BeautifulSoup(response.content, 'html.parser')
+except requests.exceptions.RequestException as e:
+    print(f"データ取得中にエラーが発生しました( {url} ): {e}")
 
 #保存先のファイルを作成
 f = open("data.txt", "w", encoding="utf-8")
